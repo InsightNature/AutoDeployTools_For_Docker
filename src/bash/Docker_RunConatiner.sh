74e387642bf8df451d6227bfa6d50a014aeea9ae
burt.liu <burt.liu@quectel.com>
2025-10-15 16:31:28 +0800

    2025/10/15:更新内容
    1. 重塑自动化部署逻辑，现在您不需要在部署完容器后手动执行容器初始化脚本
    2. 容器现在exec进入后自动切换为quectel用户，无需手动切换

---
+++ src/bash/Docker_RunConatiner.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+if [ $# -lt 3 ];
+then
+    echo "please input [container_name][container_file][image_name]"
+    exit 1
+fi
+
+container_name=$1
+container_file=$2
+image_name=$3
+sel="${4:-}"
+
+while true; do
+    read -s -p "Set user password: " user_password
+    echo
+    read -s -p "Confirm user password: " user_password_confirm
+    echo
+    if [ "$user_password" = "$user_password_confirm" ]; then
+        break
+    else
+        echo "Passwords do not match. Please try again."
+    fi
+done
+
+tee Dockerfile <<EOF > /dev/null
+FROM $image_name
+
+ENV DEBIAN_FRONTEND=noninteractive
+ENV USER=quectel
+
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends vim sudo && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*  
+
+RUN echo "alias ls='ls --color=auto'" >> /etc/bash.bashrc
+
+RUN useradd -m -s /bin/bash \$USER && \
+    echo "\$USER:$user_password" | chpasswd && \
+    usermod -aG sudo \$USER
+EOF
+
+new_image_name="${image_name}-with-user"
+docker build -t $new_image_name .
+
+if [ "$sel" = "--mnt" ]; then
+	docker run -it \
+	  --privileged \
+	  --name $container_name \
+	  -v $container_file:/home/quectel/WorkSpace \
+	  -v /opt:/opt \
+	  -v $HOME/.ssh:/home/quectel/.ssh:ro \
+	  -w /home/quectel/WorkSpace \
+	  --user quectel \
+	  $new_image_name \
+	  /bin/bash
+elif [ "$sel" = "--ssh" ]; then
+	docker run -it \
+	  --name $container_name \
+	  -v $container_file:/home/quectel/WorkSpace \
+	  -v $HOME/.ssh:/home/quectel/.ssh:ro \
+	  -w /home/quectel/WorkSpace \
+	  --user quectel \
+	  $new_image_name \
+	  /bin/bash
+else
+	docker run -it \
+	  --name $container_name \
+	  -v $container_file:/home/quectel/WorkSpace \
+	  -w /home/quectel/WorkSpace \
+	  --user quectel \
+	  $new_image_name \
+	  /bin/bash
+fi
